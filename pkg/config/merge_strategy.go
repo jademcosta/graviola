656cc80734e500e256a07176f434416d460367af
jademcosta <jademcosta@gmail.com>
2024-01-01 19:11:43 -0300

    Add merge strategy config

---
+++ pkg/config/merge_strategy.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"slices"
+)
+
+const DefaultStrategyType = "keep_biggest"
+
+type MergeStrategyConfig struct {
+	Type string `yaml:"type"`
+	// Time string `yaml:"time"` //TODO: will be used in the future when dedup by time window is implemented
+}
+
+func (mergeStratConf MergeStrategyConfig) FillDefaults() MergeStrategyConfig {
+	if mergeStratConf.Type == "" {
+		mergeStratConf.Type = DefaultStrategyType
+	}
+
+	return mergeStratConf
+}
+
+func (mergeStratConf MergeStrategyConfig) IsValid() error {
+	if !slices.Contains(listSupportedMergeStrategies(), mergeStratConf.Type) {
+		return fmt.Errorf("merge strategy type %s is invalid", mergeStratConf.Type)
+	}
+
+	return nil
+}
+
+func listSupportedMergeStrategies() []string {
+	return []string{"keep_biggest", "always_merge"}
+}
---
+++ pkg/config/merge_strategy_test.go
@@ -0,0 +1,45 @@
+package config_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/jademcosta/graviola/pkg/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAcceptSpecificMergeStrategiesNames(t *testing.T) {
+
+	testCases := []struct {
+		value       string
+		shouldError bool
+	}{
+		{"debug", true},
+		{"", true},
+		{"something", true},
+		{"keepbiggest", true},
+		{"alwaysmerge", true},
+
+		{"always_merge", false},
+		{"keep_biggest", false},
+	}
+
+	for _, tc := range testCases {
+		sut := config.MergeStrategyConfig{Type: tc.value}
+		err := sut.IsValid()
+
+		if tc.shouldError {
+			assert.Error(t, err, fmt.Sprintf("type value %s should result in error when calling Validate", tc.value))
+		} else {
+			assert.NoError(t, err, fmt.Sprintf("type value %s should NOT result in error when calling Validate", tc.value))
+		}
+	}
+}
+
+func TestMergeStrategyDefaultValues(t *testing.T) {
+	sut := config.MergeStrategyConfig{}
+	newSut := sut.FillDefaults()
+
+	assert.Equal(t, config.DefaultStrategyType, newSut.Type,
+		fmt.Sprintf("merge strategy type should be set to %s if the provided value is empty", config.DefaultStrategyType))
+}
