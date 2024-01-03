381c478c76afbb70310e7166570fd3ca0c16f799
jademcosta <jademcosta@gmail.com>
2024-01-02 21:16:02 -0300

    Implement start and stop of the app

--- pkg/application/app.go
+++ pkg/application/app.go
@@ -1,11 +1,17 @@
 package application
 
 import (
+	"context"
+	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"regexp"
+	"syscall"
 	"time"
 
-	"github.com/grafana/regexp"
+	grafanaregexp "github.com/grafana/regexp"
 	"github.com/jademcosta/graviola/pkg/config"
 	"github.com/jademcosta/graviola/pkg/graviolalog"
 	"github.com/jademcosta/graviola/pkg/querytracker"
@@ -13,6 +19,9 @@ import (
 	"github.com/jademcosta/graviola/pkg/remotestoragegroup"
 	"github.com/jademcosta/graviola/pkg/storageproxy"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/route"
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/prometheus/promql"
 	"github.com/prometheus/prometheus/storage"
@@ -27,10 +36,11 @@ type App struct {
 	api     *api_v1.API
 	logger  *slog.Logger
 	metricz *prometheus.Registry
+	srv     *http.Server
+	conf    config.GraviolaConfig // TODO: this is needed due to the api server configs
 }
 
 func NewApp(conf config.GraviolaConfig) *App {
-
 	logger := graviolalog.NewLogger(conf.LogConf)
 	metricRegistry := prometheus.NewRegistry()
 
@@ -60,13 +70,13 @@ func NewApp(conf config.GraviolaConfig) *App {
 		"",                         // dbDir string
 		false,                      // enableAdmin bool
 		graviolalog.AdaptToGoKitLogger(logger),
-		nil,                      // func(context.Context) RulesRetriever
-		1,                        //TODO: allow config (remoteReadSampleLimit)
-		1,                        //TODO: allow config (remoteReadConcurrencyLimit)
-		1024,                     //TODO: allow config (remoteReadMaxBytesInFrame) (currently 1KB)
-		false,                    // isAgent bool - If this is set to true the query endpoints will not work
-		regexp.MustCompile(".*"), // corsOrigin *regexp.Regexp,
-		nil,                      // runtimeInfo func() (RuntimeInfo, error)
+		nil,                             // func(context.Context) RulesRetriever
+		1,                               //TODO: allow config (remoteReadSampleLimit)
+		1,                               //TODO: allow config (remoteReadConcurrencyLimit)
+		1024,                            //TODO: allow config (remoteReadMaxBytesInFrame) (currently 1KB)
+		false,                           // isAgent bool - If this is set to true the query endpoints will not work
+		grafanaregexp.MustCompile(".*"), // corsOrigin *regexp.Regexp,
+		nil,                             // runtimeInfo func() (RuntimeInfo, error)
 		&web.PrometheusVersion{
 			Version:   version.Version,
 			Revision:  version.Revision,
@@ -82,19 +92,56 @@ func NewApp(conf config.GraviolaConfig) *App {
 		otlpEnabled,
 	)
 
+	router := route.New()
+
+	metricRegistry.MustRegister(
+		collectors.NewBuildInfoCollector(),
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
+		collectors.NewGoCollector(
+			collectors.WithGoCollectorRuntimeMetrics(collectors.GoRuntimeMetricsRule{Matcher: regexp.MustCompile("/.*")}),
+		),
+	)
+	router.Get("/metrics", promhttp.HandlerFor(metricRegistry, promhttp.HandlerOpts{Registry: metricRegistry}).ServeHTTP)
+
+	router = router.WithPrefix("/api/v1")
+	apiV1.Register(router)
+
+	srv := &http.Server{Addr: fmt.Sprintf(":%d", conf.ApiConf.Port), Handler: router} //TODO: extract and allow config
+
 	return &App{
 		api:     apiV1,
 		logger:  logger,
 		metricz: metricRegistry,
+		srv:     srv,
+		conf:    conf,
 	}
 }
 
 func (app *App) Start() {
+	go func() {
+		signalsCh := make(chan os.Signal, 2)
+		signal.Notify(signalsCh, syscall.SIGINT, syscall.SIGTERM)
 
+		s := <-signalsCh
+		app.logger.Info("received signal", "signal", s.String())
+		app.Stop()
+	}()
+
+	app.logger.Info("Starting server...")
+	fmt.Println(fmt.Errorf("on serving HTTP: %w", app.srv.ListenAndServe()))
 }
 
 func (app *App) Stop() {
+	ctx, cancelFn := context.WithTimeout(context.Background(), app.conf.ApiConf.TimeoutDuration())
+	defer cancelFn()
 
+	app.logger.Info("shutting down")
+
+	err := app.srv.Shutdown(ctx)
+	if err != nil {
+		app.logger.Error("error when sutting down", "error", err)
+	}
+	app.logger.Info("shutdown finished")
 }
 
 func alwaysReadyHandler(f http.HandlerFunc) http.HandlerFunc {
