7ac5f4d69fba1ec3b4febf7067adb0c391fe5df0
jademcosta <jademcosta@gmail.com>
2024-01-08 18:47:39 -0300

    Add metrics from default Go collectors

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"regexp"
 	"syscall"
 	"time"
 
@@ -18,6 +19,7 @@ import (
 	"github.com/jademcosta/graviola/pkg/remotestoragegroup"
 	"github.com/jademcosta/graviola/pkg/storageproxy"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/route"
 	"github.com/prometheus/common/version"
@@ -85,14 +87,13 @@ func NewApp(conf config.GraviolaConfig) *App {
 
 	router := route.New()
 
-	// TODO: is this needed?
-	// metricRegistry.MustRegister(
-	// 	collectors.NewBuildInfoCollector(),
-	// 	collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
-	// 	collectors.NewGoCollector(
-	// 		collectors.WithGoCollectorRuntimeMetrics(collectors.GoRuntimeMetricsRule{Matcher: regexp.MustCompile("/.*")}),
-	// 	),
-	// )
+	metricRegistry.MustRegister(
+		collectors.NewBuildInfoCollector(),
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
+		collectors.NewGoCollector(
+			collectors.WithGoCollectorRuntimeMetrics(collectors.GoRuntimeMetricsRule{Matcher: regexp.MustCompile("/.*")}),
+		),
+	)
 	router.Get("/metrics", promhttp.HandlerFor(metricRegistry, promhttp.HandlerOpts{Registry: metricRegistry}).ServeHTTP)
 	router.Get("/healthy", alwaysSuccessfulHandler)
 	router.Get("/ready", alwaysSuccessfulHandler)
